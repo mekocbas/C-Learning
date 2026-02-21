bdabceb5e42a799b09d5a8551f156a1f0563fa72
mekocbas <artvinli.enes.96@gmail.com>
2026-02-21 13:10:57 +0300

    Veri Tiplerini Yazdım

---
+++ veriTipleri.cs
@@ -0,0 +1,12 @@
+﻿/*
+    C# Veri Tipleri
+
+    Value Types:
+        Tam sayı:            byte, short, int, long
+        Ondalıklı sayılar:   float, double, decimal
+        Diğer Veri Tipleri:  char, boolean, struct
+
+    Reference Types:
+        String, Class, Array, Interface
+*/
+
